c367798c26edab93595c47297c5647fd4409ac30
Abdullah Emad <amenesy8@gmail.com>
2025-01-15 22:23:29 +0200

    Finishing meme generator project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header"
+import Main from "./components/Main"
 
-export default function Main() {
+export default function App() {
 
     return (
         <>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,39 @@
+import React from 'react'
+
 export default function Main() {
-    
-    
+    const [meme, setMeme] = React.useState({
+        topText: "Hi there",
+        bottomText: "This is a meme",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function handleChange(event){
+        const { value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [event.target.name]: value
+        }))
+    }
+    /// image state and fetching images array when app mounts
+    const [memes, setMemes] = React.useState([])
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setMemes(data.data.memes))
+    }, [])
+    ///
+    function getRandomMemeImage() {
+        const randomNumber = Math.floor(Math.random() * memes.length)
+        const url = memes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
     return (
         <main>
         <div className="form">
-        <div className="inputs">
+        <div className="inputs" onChange={handleChange}>
             <label className="input-tile">Top Text
                 <input
 
@@ -14,7 +43,7 @@ export default function Main() {
                 />
             </label>
 
-            <label className="input-tile">Bottom Text
+            <label className="input-tile" onChange={handleChange}>Bottom Text
                 <input
                     type="text"
                     placeholder="Walk into Mordor"
@@ -22,12 +51,12 @@ export default function Main() {
                 />
             </label>
         </div>
-        <button className="generate-btn">Get a new meme image 🖼</button>
+        <button className="generate-btn" onClick={getRandomMemeImage}>Get a new meme image 🖼</button>
     </div>
     <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.randomImage} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
     </div>
 </main>
     )
